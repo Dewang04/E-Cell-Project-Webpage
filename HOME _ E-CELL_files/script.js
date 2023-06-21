0314f5086a027d89e5e5e1c61705b9b8f4c79ad4
Devang Bawri <137266050+Dewang04@users.noreply.github.com>
2023-06-21 14:28:02 +0530

    added index.html

---
+++ HOME _ E-CELL_files/script.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector(".toggle");
+const navv = document.querySelector(".navList ul li");
+
+toggle.addEventListener("click", () => {
+    toggle.classList.toggle("active");
+    navv.classList.toggle("active");
+
+})
